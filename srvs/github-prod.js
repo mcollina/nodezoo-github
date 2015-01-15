2a409d700a3f9f0da5c35b7605da1a9f66ce4fd8
Matteo Collina <hello@matteocollina.com>
2015-01-15 11:52:57 +0000

    Updated to seneca 0.6.0. Added collector and influxdb.

--- srvs/github-prod.js
+++ srvs/github-prod.js
@@ -1,10 +1,17 @@
 'use strict';
 
+var redisIP = process.env.REDIS_IP || 'localhost';
+var beanstakIP = process.env.BEANSTALK_IP || 'localhost';
+var influxIP = process.env.INFLUX_IP || 'localhost';
+
 require('seneca')()
   .use('redis-transport')
-  .use('jsonfile-store',{folder:__dirname+'/../data'})
+  // disabled as not compatible with seneca 0.6 yet
+  //.use('jsonfile-store',{folder:__dirname+'/../data'})
+  .use('mem-store',{web:{dump:true}})
   .use('beanstalk-transport')
   .use('../github.js')
+  .use('collector', { host: influxIP })
   .add('role:info,req:part',function(args,done){
     done();
     this.act('role:github,cmd:get', {name:args.name}, function(err,mod){
@@ -29,8 +36,8 @@ require('seneca')()
       }
     });
   })
-  .listen({host: process.env.REDIS_IP, type:'redis',pin:'role:info,req:part'})
-  .client({host: process.env.REDIS_IP, type:'redis',pin:'role:info,res:part'})
-  .client({host: process.env.BEANSTALK_IP, port: 1130, type: 'beanstalk', pin: 'role:npm,cmd:*'})
-  .listen({host: process.env.BEANSTALK_IP, port: 1130, type: 'beanstalk', pin: 'role:github,cmd:*'});
+  .listen({host: redisIP, type:'redis',pin:'role:npm,req:part'})
+  .client({host: redisIP, type:'redis',pin:'role:github,res:part'})
+  .client({host: beanstakIP, port: 1130, type: 'beanstalk', pin: 'role:npm,cmd:*'})
+  .listen({host: beanstakIP, port: 1130, type: 'beanstalk', pin: 'role:github,cmd:*'});
 
